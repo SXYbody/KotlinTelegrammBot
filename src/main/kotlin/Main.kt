c6dd4d79058fe279796d0edf113560835bf1cb88
saveliy <saveliykislic88@mail.ru>
2025-07-28 22:21:35 +0700

    KBT-02 Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.example
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val file = File("dictionary")
+    file.createNewFile()
+
+    for (i in file.readLines()) {
+        print("$i\n")
+    }
 }
