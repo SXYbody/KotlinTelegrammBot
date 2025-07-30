d5d0885bec5607b2ebd5c8a186f236b34ad08417
saveliy <saveliykislic88@mail.ru>
2025-07-30 23:00:53 +0700

    KBT-04 Инициализация списка и добавление слов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,24 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: String? = "0",
+)
+
 fun main() {
     val file = File("dictionary")
     file.createNewFile()
 
+    val dictionaryList = mutableListOf<Word>()
+
     for (i in file.readLines()) {
-        print("$i\n")
+        val splitWord = i.split("|")
+        val word = Word(splitWord[0], splitWord[1])
+        if (splitWord.getOrNull(2) != null) dictionaryList.add(word.copy(correctAnswersCount = splitWord[2])) else dictionaryList.add(
+            word
+        )
     }
+    dictionaryList.forEach { println(it) }
 }
